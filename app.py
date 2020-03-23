acbb5e24d7b39352498067d7d80bd9c65534a79c
mtfaye <tahafaye@hotmail.com>
2020-03-23 00:00:50 -0400

    correct PEP.8 format

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from __future__ import print_function    # (at top of module)
-#from spotipy.oauth2 import SpotifyClientCredentials
+from __future__ import print_function  # (at top of module)
+from spotipy.oauth2 import SpotifyClientCredentials
 
 import os
 import sys
@@ -11,15 +11,13 @@ import spotipy.util as util
 from statistics import mean
 from json.decoder import JSONDecodeError
 
-import dashpython
+import dash
 import dash_table
 import dash_daq as daq
 import plotly.graph_objs as go
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
-
-
 
 # API ACCESS
 
@@ -34,16 +32,13 @@ except (AttributeError, JSONDecodeError):
     os.remove(f".cache-{username}")
     token = util.prompt_for_user_token(username, scope)
 
-
 # Create our Spotify object with permissions
 spotifyObject = spotipy.Spotify(auth=token)
-
 
 app = dash.Dash(__name__)
 app.config['suppress_callback_exceptions'] = True
 app.title = 'bump'
 app.colors = {'background': '#5F5958'}
-
 
 # Boostrap CSS
 app.css.append_css({'external_url': 'https://codepen.io/mtfaye/pen/MWgpoyp.css'})
@@ -55,7 +50,7 @@ app.layout = html.Div(
                 html.Div(
                     [
                         html.H1(children='bump',
-                                style={'color': 'white', 'fontSize':23, 'text-indent':10,'line-height': 50},
+                                style={'color': 'white', 'fontSize': 23, 'text-indent': 10, 'line-height': 50},
                                 className='banner'
                                 ),
 
@@ -66,8 +61,8 @@ app.layout = html.Div(
                                  )
                     ], className="row"
                 ),
-            html.Div([
                 html.Div([
+                    html.Div([
 
                         dcc.Input(id='input', value='', type='text',
                                   style={'display': 'inline-block', 'width': '30%'},
@@ -76,41 +71,40 @@ app.layout = html.Div(
                         html.Div(id='output_div'
                                  )
                     ], className="row"
-                ),
-                html.Div([
+                    ),
+                    html.Div([
 
-                    dcc.Dropdown(
-                        id='dropdown',
-                        options=[{'label': 'Acousticness', 'value': 'Acousticness'},
-                                 {'label': 'Liveness', 'value': 'Liveness'},
-                                 {'label': 'Energy', 'value': 'Energy'},
-                                 {'label': 'Valence', 'value': 'Valence'},
-                                 {'label': 'Danceability', 'value': 'Danceability'}
-                                 ],
-                        value='All',
-                        multi=True,
-                        placeholder='Choose a feature'
-                    )],className='six columns'),
-                html.Div(
-                    [
-                        dcc.Graph(
-                            id='feat-graph'
-                        )
-                    ],className='twelve columns'
-                ),
+                        dcc.Dropdown(
+                            id='dropdown',
+                            options=[{'label': 'Acousticness', 'value': 'Acousticness'},
+                                     {'label': 'Liveness', 'value': 'Liveness'},
+                                     {'label': 'Energy', 'value': 'Energy'},
+                                     {'label': 'Valence', 'value': 'Valence'},
+                                     {'label': 'Danceability', 'value': 'Danceability'}
+                                     ],
+                            value='All',
+                            multi=True,
+                            placeholder='Choose a feature'
+                        )], className='six columns'),
+                    html.Div(
+                        [
+                            dcc.Graph(
+                                id='feat-graph'
+                            )
+                        ], className='twelve columns'
+                    ),
 
-                html.Div(
-                    [
-                        dash_table.DataTable(
-                            id='table',
-                            columns=[]
-                        )
+                    html.Div(
+                        [
+                            dash_table.DataTable(
+                                id='table',
+                                columns=[]
+                            )
 
-                    ], className="six columns"
+                        ], className="six columns"
 
-
-                )
-            ],className='row')
+                    )
+                ], className='row')
                 ,
                 html.Div(
                     [
@@ -121,7 +115,7 @@ app.layout = html.Div(
                         html.A('mtfaye25@gmail.com',
                                href='mailto:mtfaye25@gmail.com'
                                )
-                    ],className="twelve columns",
+                    ], className="twelve columns",
                     style={'fontSize': 10, 'padding-top': 18}
                 )
             ], className="row"
@@ -130,13 +124,11 @@ app.layout = html.Div(
 )
 
 
-
-@app.callback(Output('output_div','children'),
+@app.callback(Output('output_div', 'children'),
               [Input('input', 'value')])
-
 def update_div(input_data):
     # Get list of tracks for a given artist
-    results = spotifyObject.search(input_data,limit=40)
+    results = spotifyObject.search(input_data, limit=40)
     # print(json.dumps(results, indent=4))
 
     # Get the list of tracks name
@@ -144,7 +136,6 @@ def update_div(input_data):
     for i, t in enumerate(results['tracks']['items']):
         # print(' ', i, t['name'])
         tids.append(t['uri'])
-
 
     # Create DataFrame for results
     df_results = pd.DataFrame(results['tracks']['items'])
@@ -157,12 +148,10 @@ def update_div(input_data):
     #  Create DataFrame for features
     df_features = pd.DataFrame(features)
 
-
     # Lets concat df_results and df_features
     df_results.reset_index(drop=True, inplace=True)
     df_features.reset_index(drop=True, inplace=True)
     global df
-
 
     df = pd.concat([df_results, df_features], axis=1)
     df = df.loc[:, ~df.columns.duplicated()]
@@ -186,108 +175,83 @@ def update_div(input_data):
     con = sqlite3.connect('spotify-data.db')
     df = pd.read_sql_query('SELECT * FROM Audio', con)
 
+    return [
+        html.Div([
+            html.Div(
 
-    return[
-          html.Div([
-              html.Div(
+                [html.P('List of analysed tracks'),
+                 html.Div(
+                     html.Table(
+                         [html.Tr([
+                             html.Th()
+                         ])] +
+                         # Body
+                         [html.Tr([html.Td(
+                             html.A(
+                                 df.iloc[i]['name'],
+                                 href=df.iloc[i]["name"],
+                                 target="_blank"
+                             )
+                         )]
+                         )
+                             for i in range(len(df.name))
+                         ]
+                     ), style={"height": "350px", "width": "100",
+                               'float': 'left', "overflowY": "scroll"}
 
-                  [html.P('List of analysed tracks'),
-                      html.Div(
-                          html.Table(
-                              [html.Tr([
-                                  html.Th()
-                              ])] +
-                              # Body
-                              [
-                                  html.Tr(
-                                      [
-                                          html.Td(
-                                              html.A(
-                                                  df.iloc[i]['name'],
-                                                  href=df.iloc[i]["name"],
-                                                  target="_blank"
-                                              )
-                                          )
-                                      ]
-                                  )
-                                  for i in range(len(df.name))
-                              ]
-                          ), style={"height": "350px", "width": "100", 'float': 'left', "overflowY": "scroll"}
+                 )
 
-                      )
+                 ], className='three columns'),
 
-                  ], className='three columns'),
+            html.Div([
+                html.P(html.H5(u"{}".format(input_data),
+                               style={'fontSize': 20, 'font-weight': 'bold', 'text-indent': 10,
+                                      'text-align': 'center'}
+                               )),
+                html.Div([
+                    daq.Gauge(
+                        id='popularity-graph',
+                        label='Popularity',
+                        color={"gradient": True,
+                               "ranges": {"lightblue": [0, 60], "blue": [60, 80], "darkblue": [80, 100]}},
+                        value=mean(list(df.popularity)),
+                        max=100,
+                        min=0,
+                        size=300
+                    )
+                ], className='six columns')
 
-                      html.Div([
-                          html.P(html.H5(u"{}".format(input_data),
-                                               style={'fontSize': 20, 'font-weight':'bold', 'text-indent':10,
-                                                      'text-align':'center'}
-            )),
-                          html.Div([
-                              daq.Gauge(
-                                  id='popularity-graph',
-                                  label='Popularity',
-                                  color={"gradient": True,
-                                         "ranges": {"lightblue": [0, 60], "blue": [60, 80], "darkblue": [80, 100]}},
-                                  value=mean(list(df.popularity)),
-                                  max=100,
-                                  min=0,
-                                  size=300
-                              )
-                          ], className='six columns')
+            ], className='nine columns')
 
-                      ],className='nine columns')
-
-          ],className='twelve columns')
+        ], className='twelve columns')
 
     ]
 
-@app.callback(Output('feat-graph','figure'),
-              [Input('dropdown','value')])
 
+@app.callback(Output('feat-graph', 'figure'),
+              [Input('dropdown', 'value')])
 def update_graph(selector):
+    options_data = {
 
-   options_data = {
+        'Acousticness': go.Scatter(x=df.acousticness, y=df.popularity, mode='markers', name='Acousticness'),
+        'Liveness': go.Scatter(x=df.liveness, y=df.popularity, mode='markers', name='Liveness'),
+        'Energy': go.Scatter(x=df.energy, y=df.popularity, mode='markers', name='Energy'),
+        'Valence': go.Scatter(x=df.valence, y=df.popularity, mode='markers', name='Valence')
+    }
 
-       'Acousticness': go.Scatter(x=df.acousticness,
-                                  y=df.popularity,
-                                 mode='markers',
-                                 name='Acousticness'
-                                 ),
-
-       'Liveness': go.Scatter(x=df.liveness,
-                              y=df.popularity,
-                             mode='markers',
-                             name='Liveness'
-                             ),
-
-        'Energy': go.Scatter(x=df.energy,
-                             y=df.popularity,
-                            mode='markers',
-                            name='Energy'
-                            ),
-
-       'Valence': go.Scatter(x=df.valence,
-                             y=df.popularity,
-                             mode='markers',
-                             name='Valence')
-             }
-
-  #  data=[]
-
-   # for i in selector:
-       data.append(
-           options_data[i]
-       )
-
-   figure={
+    data = []
+    for i in selector:
+        data.append(
+            options_data[i]
+        )
+        figure = {
             'data': data,
-            'layout':{
-                 'title': 'Track features'
-             }
+            'layout': {
+                'title': 'Track features'
+            }
 
         }
-   return figure
+    return figure
 
 
 if __name__ == '__main__':
